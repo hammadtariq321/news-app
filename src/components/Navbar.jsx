c42999f507c51e1cb5912f5a67ab1e9d4572c019
Hammad Tariq <47973106+hammadtariq321@users.noreply.github.com>
2024-05-29 20:12:46 +0500

    "Added mobile navigation menu with toggle functionality and updated navbar styling"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,55 @@
-import React from 'react'
-import { FaSearch } from "react-icons/fa";
+import React, { useState } from 'react'
+import { FaBars, FaSearch } from "react-icons/fa";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
+const nav_links = [
+    {
+        'name': "Home",
+        'link': "/",
+    },
+    {
+        'name': "Technology",
+        'link': "/technology",
+    },
+    {
+        'name': "Sports",
+        'link': "/sports",
+    }
+]
 function Navbar() {
-    const nav_links = [
-        {
-            'name': "Home",
-            'link': "/",
-        },
-        {
-            'name': "Technology",
-            'link': "/technology",
-        }
-    ]
+    const [openMenu, setOpenMenu] = useState(false)
+
+    if (openMenu) {
+        return <div className='bg-white absolute top-0 right-0 left-0 bottom-0 flex-row p-5 transition-all '>
+            <div className='flex justify-end'>
+                <IoIosCloseCircleOutline className='text-gray-600 md:hidden text-3xl text-right' onClick={() => setOpenMenu(false)} />
+            </div>
+            <div className='flex-row justify-center text-center'>
+                {nav_links?.map((item, index) => {
+                    return (
+                        <div key={index} className='text-gray-600 text-md mt-3 underline font-semibold'>{item?.name}</div>
+                    )
+                })}
+            </div>
+        </div>
+    }
     return (
-        <div className='border border-b-2 py-5 bg-[#f1f1f1] px-10'>
+        <div className='border border-b-1 py-5 px-5  bg-[#f1f1f198] md:px-10'>
             <div className='flex justify-between items-center'>
                 <div className='text-2xl font-semibold text-gray-600'>News App</div>
                 <div className='flex gap-5'>
                     {nav_links?.map((item, index) => {
                         return (
-                            <div key={index} className='text-gray-600'>{item?.name}</div>
+                            <div key={index} className='text-gray-600 text-md hidden md:block'>{item?.name}</div>
                         )
                     })}
                 </div>
-                <div>
-                    <FaSearch />
+                <div className='flex gap-2'>
+                    <FaBars className='text-gray-600 md:hidden' onClick={() => setOpenMenu(true)} />
+                    <FaSearch className='text-gray-600' />
                 </div>
             </div>
+
         </div>
     )
 }
