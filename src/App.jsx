e694b4c776c34983eddca380c1c076c3db9c95aa
Hammad Tariq <47973106+hammadtariq321@users.noreply.github.com>
2024-05-29 18:48:02 +0500

    "Added Navbar component and imported it into App component, replacing existing App content"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <main className=''>
-      <div>App</div>
+      <Navbar />
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
 function Navbar() {
+    const nav_links = [
+        {
+            'name': "Home",
+            'link': "/",
+        },
+        {
+            'name': "Technology",
+            'link': "/technology",
+        }
+    ]
     return (
-        <div>Navbar</div>
+        <div className='border border-b-2 py-5 bg-[#f1f1f1] px-10'>
+            <div className='flex justify-between items-center'>
+                <div className='text-2xl font-semibold text-gray-600'>News App</div>
+                <div className='flex gap-5'>
+                    {nav_links?.map((item, index) => {
+                        return (
+                            <div key={index} className='text-gray-600'>{item?.name}</div>
+                        )
+                    })}
+                </div>
+                <div>kasjd</div>
+            </div>
+        </div>
     )
 }
 
