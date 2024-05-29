89ee07c780556acf5990749ff708c45ceafde0f1
Hammad Tariq <47973106+hammadtariq321@users.noreply.github.com>
2024-05-29 19:02:02 +0500

    "Added react-router-dom dependency and implemented routing in main.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,23 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <div>About us</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
