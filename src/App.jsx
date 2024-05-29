c94940a01b78ed4ba9b0969341df56571a8f7207
Hammad Tariq <47973106+hammadtariq321@users.noreply.github.com>
2024-05-29 18:29:05 +0500

    Remove styles and logos from App component, simplify JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className=''>
+      <div>App</div>
+    </main>
   )
 }
 
