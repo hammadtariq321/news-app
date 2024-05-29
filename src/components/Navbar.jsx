bfeb90e81e2ab8e87d9bdcfa6d5bd516f1c89b06
Hammad Tariq <47973106+hammadtariq321@users.noreply.github.com>
2024-05-29 20:18:26 +0500

    "Updated Navbar component: replaced IoIosCloseCircleOutline with IoIosClose, added openSearch state and search input functionality"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FaBars, FaSearch } from "react-icons/fa";
-import { IoIosCloseCircleOutline } from "react-icons/io";
+import { IoIosClose } from "react-icons/io";
 
 const nav_links = [
     {
@@ -18,11 +18,12 @@ const nav_links = [
 ]
 function Navbar() {
     const [openMenu, setOpenMenu] = useState(false)
+    const [openSearch, setOpenSearch] = useState(false)
 
     if (openMenu) {
         return <div className='bg-white absolute top-0 right-0 left-0 bottom-0 flex-row p-5 transition-all '>
             <div className='flex justify-end'>
-                <IoIosCloseCircleOutline className='text-gray-600 md:hidden text-3xl text-right' onClick={() => setOpenMenu(false)} />
+                <IoIosClose className='text-gray-600 text-3xl text-right' onClick={() => setOpenMenu(false)} />
             </div>
             <div className='flex-row justify-center text-center'>
                 {nav_links?.map((item, index) => {
@@ -30,6 +31,17 @@ function Navbar() {
                         <div key={index} className='text-gray-600 text-md mt-3 underline font-semibold'>{item?.name}</div>
                     )
                 })}
+            </div>
+        </div>
+    }
+
+    if (openSearch) {
+        return <div className='bg-white absolute top-0 right-0 left-0 bottom-0 flex-row p-5 transition-all '>
+            <div className='flex justify-end'>
+                <IoIosClose className='text-gray-600 text-3xl text-right' onClick={() => setOpenSearch(false)} />
+            </div>
+            <div className='flex-row justify-center text-center'>
+                <input placeholder='Search...' className='border p-3 mt-5 rounded-lg' />
             </div>
         </div>
     }
@@ -46,7 +58,7 @@ function Navbar() {
                 </div>
                 <div className='flex gap-2'>
                     <FaBars className='text-gray-600 md:hidden' onClick={() => setOpenMenu(true)} />
-                    <FaSearch className='text-gray-600' />
+                    <FaSearch className='text-gray-600' onClick={() => setOpenSearch(true)} />
                 </div>
             </div>
 
