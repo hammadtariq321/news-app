4de6bc4b435ac156c6b4caa28d48f544f183abf5
Hammad Tariq <47973106+hammadtariq321@users.noreply.github.com>
2024-05-29 20:21:53 +0500

    "Removed `text-gray-600` class from several elements in Navbar component"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,12 +23,12 @@ function Navbar() {
     if (openMenu) {
         return <div className='bg-white absolute top-0 right-0 left-0 bottom-0 flex-row p-5 transition-all '>
             <div className='flex justify-end'>
-                <IoIosClose className='text-gray-600 text-3xl text-right' onClick={() => setOpenMenu(false)} />
+                <IoIosClose className=' text-3xl text-right' onClick={() => setOpenMenu(false)} />
             </div>
             <div className='flex-row justify-center text-center'>
                 {nav_links?.map((item, index) => {
                     return (
-                        <div key={index} className='text-gray-600 text-md mt-3 underline font-semibold'>{item?.name}</div>
+                        <div key={index} className=' text-md mt-3 underline font-semibold'>{item?.name}</div>
                     )
                 })}
             </div>
@@ -38,7 +38,7 @@ function Navbar() {
     if (openSearch) {
         return <div className='bg-white absolute top-0 right-0 left-0 bottom-0 flex-row p-5 transition-all '>
             <div className='flex justify-end'>
-                <IoIosClose className='text-gray-600 text-3xl text-right' onClick={() => setOpenSearch(false)} />
+                <IoIosClose className=' text-3xl text-right' onClick={() => setOpenSearch(false)} />
             </div>
             <div className='flex-row justify-center text-center'>
                 <input placeholder='Search...' className='border p-3 mt-5 rounded-lg' />
@@ -46,19 +46,19 @@ function Navbar() {
         </div>
     }
     return (
-        <div className='border border-b-1 py-5 px-5  bg-[#f1f1f198] md:px-10'>
+        <div className='border border-b-1 py-5 px-5   md:px-10'>
             <div className='flex justify-between items-center'>
                 <div className='text-2xl font-semibold text-gray-600'>News App</div>
                 <div className='flex gap-5'>
                     {nav_links?.map((item, index) => {
                         return (
-                            <div key={index} className='text-gray-600 text-md hidden md:block '>{item?.name}</div>
+                            <div key={index} className=' text-md hidden md:block '>{item?.name}</div>
                         )
                     })}
                 </div>
                 <div className='flex gap-2'>
-                    <FaBars className='text-gray-600 md:hidden' onClick={() => setOpenMenu(true)} />
-                    <FaSearch className='text-gray-600' onClick={() => setOpenSearch(true)} />
+                    <FaBars className=' md:hidden' onClick={() => setOpenMenu(true)} />
+                    <FaSearch className='' onClick={() => setOpenSearch(true)} />
                 </div>
             </div>
 
